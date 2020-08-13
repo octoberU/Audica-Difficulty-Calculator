d2022a6ef3e878aafbaeca07c920af1657805792
aggrogahu <microgamer2vs2@yahoo.com>
2020-08-13 16:27:03 -0700

    first commit

--- audicaCalculator.py
+++ audicaCalculator.py
@@ -2,6 +2,7 @@ import json
 import shutil
 from mido import MidiFile
 import mido
+import midi
 import os
 import csv
 from math import sqrt
@@ -48,12 +49,38 @@ def calculateAudicaMap(filename):
             if msg.type == 'set_tempo':
                 tempo = msg.tempo
 
+    #get tempos
+    def get_tempos_from_midi(midi_file):
+        pattern = midi.read_midifile("./temp/" + midiname)
+        tick = 0
+        temposList = []
+
+        for track in pattern:
+            for event in track:
+                if type(event) is midi.SetTempoEvent:
+                    tick += event.tick
+                    temposList.append({
+                        "tick": tick,
+                        "tempo": event.get_bpm()
+                        })
+        # for i, track in enumerate(pattern.tracks):
+        #     for msg in track:
+        #         if msg.type == 'set_tempo':
+        #             tempos.append({
+        #                 "tick": msg.tick,
+        #                 "tempo": msg.tempo
+        #                 })
+            return temposList
+
     print("Audica BPM: " + str(round(mido.tempo2bpm(tempo), 2)) + "\n")
 
     def getDifficultyRating(difficultyName):
         with open ("./temp/" + difficultyName) as map:
             audicamap = json.load(map)
             cues = audicamap["cues"]
+
+        midiForTempo = MidiFile("./temp/" + midiname)
+        tempos = get_tempos_from_midi(midiForTempo)
 
         mapLength = cues[-1]["tick"] / 480 * tempo / 1000000
         objectCount = 0
@@ -129,9 +156,52 @@ def calculateAudicaMap(filename):
 
             calculatedObjects += getObjectDifficulty(item)
 
+        #function to calculate time between given cues
+        def get_delta_time(cue):
+        #tempos = self.tempos
+            if len(tempos) > 1:
+                tick = cue["tick"]
+                time = 0
+                last_tempo = 0
+                last_tick = 0
+                for tempo in tempos:
+                    bpm = tempo["tempo"]
+                    t = tempo["tick"]
+                    if t != 0:
+                        if tick >= t:
+                            tick_time = 60000 / (last_tempo * 480)
+                            tick_count = t - last_tick
+                            time = time + (tick_time * tick_count)
+                            last_tempo = bpm
+                            last_tick = t
+                        else:
+                            break
+                    else:
+                        last_tempo = bpm
+                difference = tick - last_tick
+                if difference != 0:
+                    tick_time = 60000 / (last_tempo * 480)
+                    time = time + (tick_time * difference)
+                return time
+            else:
+                return cue["tick"] / 480 * tempos[0]["tempo"]
+
+        #get first and last cues
+
+        firstCueTime = get_delta_time(finalCuesSorted[0])
+        lastCueTime = get_delta_time(finalCuesSorted[-1])
+
+        print("1st: " + str(firstCueTime))
+        print("Last: " + str(lastCueTime))
+
+        print("prev mapLength: " + str(mapLength))
+
+        mapLength = (lastCueTime - firstCueTime) / 1000
+
+        print("cur mapLength: " + str(mapLength))
 
         NPS = round((objectCount / mapLength), 2)
-        StarRating = str( round((calculatedObjects / mapLength), 2))# + "★"  
+        StarRating = str( round((calculatedObjects / mapLength), 2))
         diffname = difficultyName.capitalize().replace(".cues", "")
 
         print("Difficulty: " + diffname)
